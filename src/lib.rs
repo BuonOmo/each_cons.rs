c6e600df10bf99035e6abfa6882526f2317a5384
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2022-03-29 13:28:18 +0200

    variable length

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,69 @@
-use std::{iter::Peekable, rc::Rc};
+// TODO: get rid of peekable
+use std::{collections::VecDeque, iter::Peekable, rc::Rc};
 
 pub trait ConsExt: Sized + Iterator {
-	 fn each_cons(self) -> Cons<Self>;
+	 fn each_cons(self, slice: usize) -> Cons<Self>;
 }
 
-pub fn each_cons<I: Iterator>(iter: I) -> Cons<I> {
-	Cons::new(iter)
+pub fn each_cons<I: Iterator>(slice: usize, iter: I) -> Cons<I> {
+	Cons::new(iter, slice)
 }
 
 pub struct Cons<I: Iterator> {
-	prev: Rc<I::Item>,
+	slice: usize,
+	prevs: Option<VecDeque<Rc<I::Item>>>,
 	iter: Peekable<I>,
 }
 
 impl<I: Iterator> ConsExt for I {
-	fn each_cons(self) -> Cons<Self> {
-		Cons::new(self)
+	fn each_cons(self, slice: usize) -> Cons<Self> {
+		Cons::new(self, slice)
 	}
 }
 
 impl<I: Iterator> Cons<I> {
-	pub fn new(iter: I) -> Self {
+	pub fn new(iter: I, slice: usize) -> Self {
 		let mut peekable = iter.peekable();
-		let prev = if let Some(n) = peekable.next() {
-			Rc::new(n)
-		} else {
-			todo!("There may be a better solution for that");
-		};
-		Self { iter: peekable , prev }
+		let mut expected_prevs: VecDeque<Rc<I::Item>> = VecDeque::with_capacity(slice - 1);
+		let mut finished = true;
+		for _ in 0..(slice - 1) {
+			if let Some(val) = peekable.next() {
+				expected_prevs.push_back(Rc::new(val));
+			} else {
+				finished = false;
+				break
+			}
+		}
+		let prevs = if finished { Some(expected_prevs) } else { None };
+		Self { iter: peekable , prevs, slice }
 	}
 }
 
 impl<I: Iterator> Iterator for Cons<I> {
-	type Item = (Rc<I::Item>, Rc<I::Item>);
+	type Item = Vec<Rc<I::Item>>;
 
 	fn next(&mut self) -> Option<Self::Item> {
-		if let Some(a) = self.iter.next() {
-			let x = Rc::clone(&self.prev);
-			let y = Rc::new(a);
-			self.prev = Rc::clone(&y);
-			Some((x, y))
+		if let Some(prevs) = &self.prevs { // TODO: rewrite with .map, just to learn it
+			if let Some(a) = self.iter.next() {
+				let new_rc = Rc::new(a);
+				let mut rv: Vec<Rc<I::Item>> = Vec::with_capacity(self.slice);
+				let mut curr = VecDeque::with_capacity(self.slice - 1);
+				let mut first = true;
+				for v in prevs {
+					if first {
+						first = false
+					} else {
+						curr.push_back(Rc::clone(&v));
+					}
+					rv.push(Rc::clone(&v));
+				}
+				rv.push(Rc::clone(&new_rc));
+				curr.push_back(new_rc);
+				self.prevs = Some(curr);
+				Some(rv)
+			} else {
+				None
+			}
 		} else {
 			None
 		}
@@ -54,12 +78,14 @@ mod tests {
 	fn each_cons_function() {
 		let v = vec![1, 2, 3];
 
-		let mut i = each_cons(v.iter());
+		let mut i = each_cons(2, v.iter());
 
-		let (a, b) = i.next().unwrap();
-		assert_eq!((&1, &2), (*a, *b));
-		let (a, b) = i.next().unwrap();
-		assert_eq!((&2, &3), (*a, *b));
+		let s = i.next().unwrap();
+		assert_eq!(*s[0], &1);
+		assert_eq!(*s[1], &2);
+		let s = i.next().unwrap();
+		assert_eq!(*s[0], &2);
+		assert_eq!(*s[1], &3);
 		assert!(i.next().is_none());
 	}
 
@@ -67,12 +93,14 @@ mod tests {
 	fn each_cons_iterator_method() {
 		let v = vec![1, 2, 3];
 
-		let mut i = v.iter().each_cons();
+		let mut i = v.iter().each_cons(2);
 
-		let (a, b) = i.next().unwrap();
-		assert_eq!((&1, &2), (*a, *b));
-		let (a, b) = i.next().unwrap();
-		assert_eq!((&2, &3), (*a, *b));
+		let s = i.next().unwrap();
+		assert_eq!(*s[0], &1);
+		assert_eq!(*s[1], &2);
+		let s = i.next().unwrap();
+		assert_eq!(*s[0], &2);
+		assert_eq!(*s[1], &3);
 		assert!(i.next().is_none());
 	}
 
@@ -80,12 +108,14 @@ mod tests {
 	fn non_copy_object() {
 		let v = vec!["foo", "bar", "baz"];
 
-		let mut i = v.iter().each_cons();
+		let mut i = v.iter().each_cons(2);
 
-		let (a, b) = i.next().unwrap();
-		assert_eq!((&"foo", &"bar"), (*a, *b));
-		let (a, b) = i.next().unwrap();
-		assert_eq!((&"bar", &"baz"), (*a, *b));
+		let s = i.next().unwrap();
+		assert_eq!(*s[0], &"foo");
+		assert_eq!(*s[1], &"bar");
+		let s = i.next().unwrap();
+		assert_eq!(*s[0], &"bar");
+		assert_eq!(*s[1], &"baz");
 		assert!(i.next().is_none());
 	}
 }
